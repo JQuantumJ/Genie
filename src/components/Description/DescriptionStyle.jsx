cba59fec0f24855d14243f0b8691bd65dfeff488
yengeun <7613ez@gmail.com>
2024-09-24 17:46:05 +0900

    Chore:활용디자인 미디어쿼리 수정

--- src/components/Description/DescriptionStyle.jsx
+++ src/components/Description/DescriptionStyle.jsx
@@ -110,18 +110,19 @@ export const ConceptWrapper = styled.div`
   @media (max-width: 700px) {
     margin-top: 16%;
     padding: 4%;
+
     iframe{
     width:200px;
     height:200px;
     margin-left: 12%;}
-  }
-  .accordion-content {
-    font-size:1.3rem;
-    padding:4%;
-    }
-  a{
-    color: #007bff;
-    font-size: 1.7rem;
-    padding-left: 40%;
+  
+    .accordion-content {
+      font-size:1.3rem;
+      padding:4%;
+      }
+     a{
+      color: #007bff;
+      font-size: 1.7rem;
+      padding-left: 40%;
   }
 `;
