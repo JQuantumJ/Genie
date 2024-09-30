6ffad889a82637aa21e201005899513bb774ed9d
yengeun <7613ez@gmail.com>
2024-09-30 14:38:53 +0900

    FIX:빌드오류 테스트

---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,9 @@
+const Company = () => {
+    return (
+      <div>
+        {/* Company 컴포넌트 내용 */}
+      </div>
+    );
+  };
+  
+  export default Company; // default export 확인
---
+++ src/components/Recent/Recent.jsx
@@ -0,0 +1,9 @@
+const Recent = () => {
+    return (
+      <div>
+        {/* Company 컴포넌트 내용 */}
+      </div>
+    );
+  };
+  
+  export default Recent; // default export 확인
