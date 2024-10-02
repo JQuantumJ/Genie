ee01c23b78ca2188a39f77a2c4248db667a9a274
yengeun <7613ez@gmail.com>
2024-10-02 17:50:25 +0900

    Feat:주요기업페이지 세팅

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import * as C from './CompanyStyle';
+
 const Company = () => {
-    return (
+  return (
+      <C.Wrapper>
+      <C.ConceptWrapper>
       <div>
-        {/* Company 컴포넌트 내용 */}
+          <h1>양자 관련 주요 기업</h1>
       </div>
+      </C.ConceptWrapper> 
+      </C.Wrapper>
     );
-  };
+};
   
-  export default Company; // default export 확인
+export default Company;
