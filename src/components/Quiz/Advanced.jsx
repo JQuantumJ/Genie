8cab78ca33f619b742fe099da1af5269d94b8a7e
yengeun <7613ez@gmail.com>
2024-10-11 10:26:46 +0900

    Feat:퀴즈 문제

--- src/components/Quiz/Advanced.jsx
+++ src/components/Quiz/Advanced.jsx
@@ -6,12 +6,12 @@ import Genie2 from '/src/assets/image/main/Genie2.svg'; // Genie2 이미지 impo
 
 const Advanced = () => {
     const questions = [
-      { question: '', correctAnswer: '', explanation: ' ' },
-      { question: '', correctAnswer: 'O', explanation: '' },
-      { question: '', correctAnswer: 'O', explanation: '' },
-      { question: '', correctAnswer: 'X', explanation: '' },
-      { question: '', correctAnswer: 'X', explanation: ' ' },
-      { question: '', correctAnswer: 'X', explanation: '' }
+      { question: '양자 게이트는 고전적인 논리 게이트와 동일한 방식으로 동작한다', correctAnswer: 'X', explanation: '양자 게이트는 큐비트의 상태를 변화시키는 양자 연산을 수행하며, 고전적인 논리 게이트와는 다른 방식으로 동작합니다.' },
+      { question: '양자 컴퓨터는 오류가 발생할 가능성이 고전 컴퓨터보다 높다.', correctAnswer: 'O', explanation: '양자 컴퓨터는 큐비트의 민감한 상태 때문에 오류가 발생할 가능성이 높습니다. 이를 해결하기 위해 오류 정정 기술이 연구되고 있습니다.' },
+      { question: '양자 상태의 코히런스(coherence)는 환경과의 상호작용에 의해 손실되며, 이는 양자 디코히런스(decoherence)라고 불린다.', correctAnswer: 'O', explanation: '코히런스는 양자 상태가 다른 상태들과 정렬된 특성을 말합니다.환경과의 상호작용에 의해 이 정렬이 깨지면 디코히런스가 발생하여 양자 상태의 정보를 잃게 됩니다.' },
+      { question: '양자 컴퓨터의 성능은 큐비트의 개수에 비례하여 선형적으로 증가한다', correctAnswer: 'X', explanation: '큐비트가 추가될수록 양자 컴퓨터의 성능은 기하급수적으로 증가하며, 선형적으로 증가하는 것이 아닙니다.' },
+      { question: '양자 컴퓨터에서 큐비트의 상태를 측정하는 것은 양자 얽힘을 유지한 채로 이루어질 수 있다.', correctAnswer: 'X', explanation: '큐비트의 상태를 측정하면 양자 얽힘 상태가 붕괴됩니다. 얽힌 상태에서는 측정이 이루어지면 더 이상 얽힌 상태를 유지할 수 없습니다. ' },
+      { question: '양자 상호작용에서는 양자 터널링 효과를 이용해 에너지 장벽을 넘지 않고도 전자가 이동할 수 있다.', correctAnswer: 'O', explanation: ' 양자 터널링은 고전 물리학적으로 넘을 수 없는 에너지 장벽을 양자역학적으로 입자가 "통과"할 수 있는 현상입니다. 이 현상은 반도체와 같은 분야에서도 중요한 역할을 합니다.' }
     ];
   
 
--- src/components/Quiz/Medium.jsx
+++ src/components/Quiz/Medium.jsx
@@ -6,12 +6,12 @@ import Genie2 from '/src/assets/image/main/Genie2.svg'; // Genie2 이미지 impo
 
 const Medium = () => {
   const questions = [
-    { question: '', correctAnswer: '', explanation: ' ' },
-    { question: '', correctAnswer: 'O', explanation: '' },
-    { question: '', correctAnswer: 'O', explanation: '' },
-    { question: '', correctAnswer: 'X', explanation: '' },
-    { question: '', correctAnswer: 'X', explanation: ' ' },
-    { question: '', correctAnswer: 'X', explanation: '' }
+    { question: '양자 얽힘 상태에 있는 두 입자는 한 입자의 상태가 바뀌면 즉시 다른 입자의 상태도 바뀐다.', correctAnswer: 'O', explanation: '양자 얽힘은 두 입자가 서로 먼 거리에 떨어져 있어도 서로 연결된 상태를 유지하는 현상입니다. 한 입자의 상태가 변화하면 즉시 다른 입자의 상태도 바뀝니다. ' },
+    { question: '불확정성 원리는 입자의 위치와 속도를 동시에 정확히 측정할 수 있다는 이론이다', correctAnswer: 'X', explanation: '불확정성 원리는 입자의 위치와 속도를 동시에 정확히 측정할 수 없다는 원리입니다. 위치를 정확히 알수록 속도에 대한 정보는 불확실해집니다.' },
+    { question: '양자 상태는 외부 관측이 이루어지면 즉시 붕괴된다.', correctAnswer: 'O', explanation: '양자 상태는 관측에 의해 결정됩니다. 관측이 이루어지면 입자의 상태가 고정되어 양자 중첩 상태는 붕괴됩니다.' },
+    { question: '양자 터널링은 입자가 에너지를 충분히 갖고 있을 때만 장벽을 통과할 수 있는 현상이다.', correctAnswer: 'X', explanation: '양자 터널링은 입자가 에너지 장벽을 넘을 수 없을 때도 통과하는 현상입니다. 이는 고전적인 물리학으로 설명할 수 없는 양자적 현상입니다.' },
+    { question: '양자 얽힘을 이용하면 정보를 빛의 속도보다 빠르게 전달할 수 있다', correctAnswer: 'X', explanation: '양자 얽힘은 입자의 상태 변화를 즉시 반영하지만, 이를 통해 실제 정보를 빛의 속도보다 빠르게 전달할 수는 없습니다.' },
+    { question: '양자역학에서 파동함수는 입자의 위치를 정확히 예측해준다', correctAnswer: 'X', explanation: '파동함수는 입자의 위치를 정확히 예측하는 것이 아니라, 입자가 특정 위치에 있을 확률을 나타냅니다.' }
   ];
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -52,7 +52,7 @@ const Medium = () => {
           <h2>퀴즈 완료!</h2>
           <p>총 <span style={{color: '#0015ff'}}>{questions.length}문제</span> 중 <span style={{color: '#0015ff'}}>{score}문제</span> 맞추셨습니다.</p>
           {/* 점수에 따라 이미지를 조건부로 렌더링 */}
-          {score < 5 ? (
+          {score < 4 ? (
             <img src={Genie1} alt="Genie1" />
           ) : (
             <img src={Genie2} alt="Genie2" />
