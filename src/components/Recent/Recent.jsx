dd16987f83b7a9b3b2edb51f2541533025f17113
yengeun <7613ez@gmail.com>
2024-10-28 10:27:05 +0900

    Feat:양자 기사 업데이트/10/28

--- src/components/Recent/Recent.jsx
+++ src/components/Recent/Recent.jsx
@@ -4,16 +4,14 @@ import * as R from './RecentStyle';
 
 const Recent = () => {
     const articles = [
+        { title: "부산시, 포스코와 양자컴퓨팅 기반 미래 혁신소재 협력", thumbnail: "https://img8.yna.co.kr/photo/cms/2023/07/12/81/PCM20230712000081990_P4.jpg", url: "https://www.yna.co.kr/view/AKR20241028017000051?input=1195m" },
+        { title: "LGU+, 다계층 양자내성암호 장비 개발", thumbnail: "https://image.newsis.com/2024/10/25/NISI20241025_0001685555_web.jpg?rnd=20241025083746", url: "https://www.newsis.com/view/NISX20241025_0002933627" }, 
+        { title: "네덜란드, 12월부터 양자기술 관련 제품 수출통제", thumbnail: "https://img8.yna.co.kr/photo/etc/af/2024/10/12/PAF20241012110701009_P4.jpg", url: "https://www.yna.co.kr/view/AKR20241020055700098?input=1195m" },
+        { title: "GIST-IBS, '강력장 양자전기역학 현상' 세계 최초 입증", thumbnail: "https://image.newsis.com/2024/10/21/NISI20241021_0001681737_web.jpg?rnd=20241021141711", url: "https://www.newsis.com/view/NISX20241021_0002927863" },
+        { title: "정부, 노벨상 수상자 알랭 아스페 교수와 양자기술 협업 나서", thumbnail: "https://image.newsis.com/2024/10/22/NISI20241022_0001683269_web.jpg?rnd=20241022214821", url: "https://www.newsis.com/view/NISX20241022_0002930122" },
         { title: "SKT, 양자암호 양대 기술 QKD PQC 하나로 묶었다", thumbnail: "https://cdn.digitaltoday.co.kr/news/photo/202410/537045_501813_1137.jpg", url: "https://www.digitaltoday.co.kr/news/articleView.html?idxno=537045" },
         { title: "노르마 “양자 SW-HW 기술 연동해 풀스택 기업 도약”", thumbnail: "https://cdn.enewstoday.co.kr/news/photo/202410/2186172_993108_5327.jpg", url: "https://www.enewstoday.co.kr/news/articleView.html?idxno=2186172" },
         { title: "한국 양자기술 수준 주요국 중 최하위…양자컴은 100점 기준 2.3점", thumbnail: "https://image.dongascience.com/Photo/2024/06/5889cdb1752d35425c5c96fb03f9acb0.jpg", url: "https://m.dongascience.com/news.php?idx=66161" },
-        { title: "정부, 양자산업 주도할 국무총리급 거버넌스 만든다", thumbnail: "https://image.zdnet.co.kr/2024/10/11/dadff46fd0a6b54d8c820aeafd8a5db1.jpg", url: "https://zdnet.co.kr/view/?no=20241011235409" }, 
-        { title: "세계 최초 하이브리드 양자 오류정정 기술 개발", thumbnail: "https://image.dongascience.com/Photo/2024/10/b5179352882605f04640c1b8edce03d1.png", url: "https://www.dongascience.com/news.php?idx=67764" },
-        { title: "불교와 만난 양자역학…“집착·걱정 버리고 현재에 충실하라”", thumbnail: "https://img1.yna.co.kr/etc/inner/KR/2024/10/10/AKR20241010085300005_01_i_P4.jpg", url: "https://www.yna.co.kr/view/AKR20241010085300005?input=1195m" },
-        { title: "[임수연의 이과감성] 양자역학과 다중우주론의 온기", thumbnail: "https://image.cine21.com/resize/cine21/movie/2024/0927/16_44_30__66f6625e2ac97[W680-].jpg", url: "http://www.cine21.com/news/view/?mag_id=105904&utm_source=naver&utm_medium=news" },
-        { title: "‘양자 컴퓨팅·AI’, 데이터와 물리적 보안 강화 기반으로 부상", thumbnail: "https://cdn.gttkorea.com/news/photo/202409/13652_14786_5153.jpg", url: "https://www.gttkorea.com/news/articleView.html?idxno=13652" },
-       
-        
     ];
 
   return (
